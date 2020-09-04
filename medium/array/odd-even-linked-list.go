c334c1620c6a02a6b64aba3bd4d86146a8102da2
morganxf <xufeibuaa@163.com>
2020-09-04 11:32:04 +0800

    feat: add 奇偶链表

---
+++ medium/array/odd-even-linked-list.go
@@ -0,0 +1,27 @@
+package array
+
+func oddEvenList(head *ListNode) *ListNode {
+	return OddEvenList(head)
+}
+
+func OddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	odd, oddCur := head, head
+	even, evenCur := head.Next, head.Next
+	for oddCur != nil && evenCur != nil && oddCur.Next !=nil && evenCur.Next != nil{
+		oddCur.Next = oddCur.Next.Next
+		evenCur.Next = evenCur.Next.Next
+		oddCur = oddCur.Next
+		evenCur = evenCur.Next
+	}
+	if oddCur != nil && oddCur.Next != nil {
+		oddCur.Next = nil
+	}
+	if evenCur != nil && evenCur.Next != nil {
+		evenCur.Next = nil
+	}
+	oddCur.Next = even
+	return odd
+}
